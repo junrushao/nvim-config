09d118987ccee518913b925bbcc0a10913d00cda
Junru Shao <junrushao1994@gmail.com>
2022-07-04 22:04:59 -0700

    Rely on `CONDA_PREFIX` in `path`

--- lua/user/path.lua
+++ lua/user/path.lua
@@ -1,10 +1,32 @@
-local CONDA_PATH_PREFIX = "/root/miniconda/envs/python38"
+local CONDA_PREFIX = os.getenv("CONDA_PREFIX")
+
+function file_exists(name)
+  local f = io.open(name,"r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
+end
+
+
+local function get_conda_executable(relative_path, fallback)
+  if CONDA_PREFIX ~= nil then
+    local path = CONDA_PREFIX .. "/" .. relative_path
+    if file_exists(path) then
+      return path
+    end
+  end
+  return fallback
+end
+
 
 return {
-  PYLINT          = CONDA_PATH_PREFIX .. "/bin/pylint",
-  BLACK           = CONDA_PATH_PREFIX .. "/bin/black",
-  MYPY            = CONDA_PATH_PREFIX .. "/bin/mypy",
-  ISORT           = CONDA_PATH_PREFIX .. "/bin/isort",
+  PYLINT          = get_conda_executable("bin/pylint", "pylint"),
+  BLACK           = get_conda_executable("bin/black", "black"),
+  MYPY            = get_conda_executable("bin/mypy", "mypy"),
+  ISORT           = get_conda_executable("bin/isort", "isort"),
   CLANGD          = "clangd-14",
   CSPELL          = "cspell",
   CSPELL_DICT     = vim.fn.stdpath("config") .. "/cspell/dict.txt",
