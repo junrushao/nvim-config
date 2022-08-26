7769e7b9b9459d76ba5a1c3303f7d420f8f98965
Junru Shao <junrushao1994@gmail.com>
2022-08-25 18:57:19 -0700

    Update illuminate

--- lua/user/plugins/vim-illuminate.lua
+++ lua/user/plugins/vim-illuminate.lua
@@ -1,9 +1,39 @@
-vim.g.Illuminate_delay = 5
-vim.cmd [[ hi def link LspReferenceText CursorLine ]]
-vim.cmd [[ hi def link LspReferenceWrite CursorLine ]]
-vim.cmd [[ hi def link LspReferenceRead CursorLine ]]
-
-vim.cmd('augroup illuminate_augroup')
-vim.cmd('autocmd!')
-vim.cmd([[autocmd VimEnter * hi link illuminatedWord Visual]])
-vim.cmd('augroup END')
+-- default configuration
+require('illuminate').configure({
+    -- providers: provider used to get references in the buffer, ordered by priority
+    providers = {
+        -- 'lsp',
+        'treesitter',
+        'regex',
+    },
+    -- delay: delay in milliseconds
+    delay = 30,
+    -- filetype_overrides: filetype specific overrides.
+    -- The keys are strings to represent the filetype while the values are tables that
+    -- supports the same keys passed to .configure except for filetypes_denylist and filetypes_allowlist
+    filetype_overrides = {},
+    -- filetypes_denylist: filetypes to not illuminate, this overrides filetypes_allowlist
+    filetypes_denylist = {
+        'dirvish',
+        'fugitive',
+    },
+    -- filetypes_allowlist: filetypes to illuminate, this is overriden by filetypes_denylist
+    filetypes_allowlist = {},
+    -- modes_denylist: modes to not illuminate, this overrides modes_allowlist
+    modes_denylist = {},
+    -- modes_allowlist: modes to illuminate, this is overriden by modes_denylist
+    modes_allowlist = {},
+    -- providers_regex_syntax_denylist: syntax to not illuminate, this overrides providers_regex_syntax_allowlist
+    -- Only applies to the 'regex' provider
+    -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+    providers_regex_syntax_denylist = {},
+    -- providers_regex_syntax_allowlist: syntax to illuminate, this is overriden by providers_regex_syntax_denylist
+    -- Only applies to the 'regex' provider
+    -- Use :echom synIDattr(synIDtrans(synID(line('.'), col('.'), 1)), 'name')
+    providers_regex_syntax_allowlist = {},
+    -- under_cursor: whether or not to illuminate under the cursor
+    under_cursor = true,
+})
+vim.cmd [[ hi def link IlluminatedWordText Visual ]]
+vim.cmd [[ hi def link IlluminatedWordRead Visual ]]
+vim.cmd [[ hi def link IlluminatedWordWrite Visual ]]
